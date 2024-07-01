f323b75244d4fe34003cd34e13602e099a7eb82d
GioGioCa <darkconor2500@hotmail.com>
2024-06-30 23:28:46 -0600

    Feat(Pages/Note_Pages): Agregada funcionalidad para creacion de notas (UI incluida))

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:lista/models/note_database.dart';
+import 'package:provider/provider.dart';
 
 class NotesPage extends StatefulWidget {
-  const NotesPage({ super.key });
+  const NotesPage({super.key});
 
   @override
   State<NotesPage> createState() => _NotesPageState();
 }
 
 class _NotesPageState extends State<NotesPage> {
+  //Text controller to access what the user typed
+  final textController = TextEditingController();
+
+  //Create a note
+  void createNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          //Create button
+          MaterialButton(
+            onPressed: () {
+              //Add to db
+              context.read<NoteDatabase>().addNote(textController.text);
+
+              //Pop up dialog
+              Navigator.pop(context);
+            },
+            child: const Text("Create"),
+          )
+        ],
+      ),
+    );
+  }
+
+  //Read a note
+
+  //Update a note
+
+  //Delete a note
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
